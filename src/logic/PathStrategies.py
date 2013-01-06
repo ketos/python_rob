765dcb12f5ab2c2659acd71ceec7f6cd5591d2ba
Tim Wessels <timwess@gmail.com>
2013-01-06 18:23:05 +0100

    +pathstrategies

---
+++ src/logic/PathStrategies.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 28.11.2012
+
+@author: tim
+'''
+
+class PathStrategies(object):
+    
+    def __init__(self):
+        pass
+        
+    def RightHand(self):
+        pass
+        
+    def LeftHand(self):
+        pass
+        
+    def inVoid(self):
+        # Definiert verhalten im freiraum
+        pass
